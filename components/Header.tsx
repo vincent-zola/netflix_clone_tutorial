574d3a1076761322ef5fce6bb7bf90cf0e29994e
vincent-zola <vincent.del@protonmail.com>
2022-04-17 07:21:46 +0200

    skeleton

---
+++ components/Header.tsx
@@ -0,0 +1,65 @@
+// *========== Imports ==========
+
+import { BellIcon, SearchIcon } from '@heroicons/react/solid'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+
+// *========== Variables & Functions ==========
+
+const Header = () => {
+  // for checking is Scrollbar was moved
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    //   Fn. checks if user scrolled
+    const handleScroll = () => {
+        // check if window was scrolled
+        if(window.scrollY > 0) {
+            setIsScrolled(true)
+        }else{
+            setIsScrolled(false)
+        }
+    }
+  }, [])
+
+  // *========== HTML ==========
+  return (
+    <header>
+      {/* tailwind is mobil first, so we need to write "md:" to reflect desktop view */}
+      {/* space: Control the horizontal space between elements */}
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <img
+          //! img src from wikipedia, needs change
+          src="https://rb.gy/ulxxee"
+          //   width is in px
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="hidden h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+        <Link href="/account">
+          <img
+            //! img src unknown, needs change
+            src="https://rb.gy/g1pwyx"
+            alt="account link"
+            className="cursor-pointer rounded"
+          />
+        </Link>
+      </div>
+    </header>
+  )
+}
+
+export default Header
